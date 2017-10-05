dd41cfb302889b8ad2162d19c365eeaf70c60d21
Allan Lukwago <epicallan.al@gmail.com>
2017-10-05 10:52:41 +0300

    send email and refactoring

--- src/Handlers.hs
+++ src/Handlers.hs
@@ -1,3 +1,10 @@
 -- | routes handlers
 module Handlers where
 
+
+
+-- for deploy route / deploy route
+-- config for sending emails
+-- deploy :: Repo -> ReaderT AppConf IO (Either String String)
+
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,20 +4,25 @@
 module Lib (
         isDomainUp
     ,   runDeployBash
-    ,   readConf) where
-import           Control.Exception    (SomeException, try)
-import           Data.Aeson           (eitherDecode)
-import qualified Data.ByteString.Lazy as B
-import           Data.Maybe           (fromMaybe)
-import qualified Data.Text            as T
-import           Data.Text.IO         (hGetContents)
-
-import           Network.HTTP.Simple  (Request, getResponseStatusCode, httpLBS)
+    ) where
+import           Control.Exception          (SomeException, try)
+import           Control.Monad.Trans.Reader
+import           Data.Aeson                 (eitherDecode)
+import qualified Data.ByteString.Lazy       as B
+import           Data.List                  (find)
+import           Data.Maybe                 (fromMaybe)
+import qualified Data.Text                  as T
+import           Data.Text.IO               (hGetContents)
+import qualified Data.Text.Lazy             as TL
+import           Network.HaskellNet.Auth
+import           Network.HaskellNet.SMTP
+import           Network.HTTP.Simple        (Request, getResponseStatusCode,
+                                             httpLBS)
 import           Prelude
-import           System.Process       (CreateProcess (..),
-                                       StdStream (CreatePipe), createProcess,
-                                       proc)
-import           Types                (AppConf)
+import           System.Process             (CreateProcess (..),
+                                             StdStream (CreatePipe),
+                                             createProcess, proc)
+import           Types                      (EmailConf (..), Repo (..))
 
 isDomainUp :: Request -> IO (Either String String)
 isDomainUp url = do
@@ -31,10 +36,10 @@ isDomainUp url = do
                     | statusCode == 400  = Right "site is down"
                     | otherwise = Right "site might be down, please check it"
 
-readConf :: FilePath -> IO (Either String AppConf)
-readConf jsonFile = do
-    bJson <- B.readFile jsonFile
-    return (eitherDecode bJson :: Either String AppConf)
+-- readConf :: FilePath -> IO (Either String AppConf)
+-- readConf jsonFile = do
+--     bJson <- B.readFile jsonFile
+--     return (eitherDecode bJson :: Either String AppConf)
 
 
 runDeployBash :: String -> Maybe [String] -> IO T.Text
@@ -43,3 +48,21 @@ runDeployBash bashCmds args = do
     case outHandle of
         Nothing     -> return "Failed to get handle, command possibly didnt run"
         Just handle -> hGetContents handle
+
+
+
+-- send email
+sendEmail :: EmailConf -> IO ()
+sendEmail conf = doSMTPPort (smtpServer conf) 587 $ \conn -> do
+    authSucceed <- authenticate PLAIN (email conf) (password conf) conn
+    if authSucceed
+        then sendPlainTextMail "receiver@server.com" "sender@server.com" "subject" (TL.pack "Hello! This is the mail body!") conn
+        else print "Authentication failed."
+
+
+-- get repo; this should be a search in the db. currently we will look in a json file
+getRepoToDeploy :: String -> [Repo] -> Maybe Repo
+getRepoToDeploy reponame = find (\(Repo name _ _ ) -> name == reponame)
+
+-- stream bash output
+--- handle -> IO Either String String
